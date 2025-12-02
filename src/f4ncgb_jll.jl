cae1b15fa7143dbfd2345440474b18b3c68313d2
jlbuild <juliabuildbot@gmail.com>
2025-12-02 09:59:19 +0000

    f4ncgb_jll build 0.300.300+0

--- src/f4ncgb_jll.jl
+++ src/f4ncgb_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("f4ncgb")
-JLLWrappers.@generate_main_file("f4ncgb", UUID("160f184c-7e1a-5d95-af1f-70d62a450302"))
+JLLWrappers.@generate_main_file("f4ncgb", Base.UUID("160f184c-7e1a-5d95-af1f-70d62a450302"))
 end  # module f4ncgb_jll
